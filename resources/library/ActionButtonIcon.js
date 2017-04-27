dd6af87ac25bf8afca1b4d0618171c96e822a0b0
SBI- <pascal.huerlimann@gmail.com>
2017-04-27 17:55:29 +0200

    Fixed identation.

--- resources/library/ActionButtonIcon.js
+++ resources/library/ActionButtonIcon.js
@@ -11,7 +11,7 @@ define([
             var self = this;
             var backgroundStyle = 'rgba(0, 0, 0, 0) url("' + net.jazz.ajax._contextRoot + self._url + '") no-repeat scroll -96px -96px';
 
-			var observer = new MutationObserver(function(mutations) {
+            var observer = new MutationObserver(function(mutations) {
                 mutations.filter(function(mutation) {
                     return mutation.target.title === self._label;
                 }).forEach(function(mutation) {
@@ -20,10 +20,10 @@ define([
                         node.style.background = backgroundStyle;
                     });
                 });
-			});
+            });
 
-			var config = { attributes: true, childList: true, characterData: true, subtree: true };
-			observer.observe(document.body, config);
+            var config = { attributes: true, childList: true, characterData: true, subtree: true };
+            observer.observe(document.body, config);
         }
     });
 });

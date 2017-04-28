b5cd4f14f1f8b1eff2250351de6785eccb8afdaf
SBI- <pascal.huerlimann@gmail.com>
2017-04-28 14:32:31 +0200

    Added fallback icon when certain browser functions aren't available. Explanation added to code.

--- resources/library/ActionButtonIcon.js
+++ resources/library/ActionButtonIcon.js
@@ -8,8 +8,24 @@ define([
         },
 
         attach: function() {
+            // Check IE compatibility. For some reason, IBM sets the X-UA-Compatible header to
+            // IE-10, which makes ie behave to ancient standards. Chrome, FF etc. just ignore
+            // this header and are then fine with rendering new css rules and using new js features.
+            // IE11 actually supports MutationObserver, so it's a shame we have to do this.
+            // (https://msdn.microsoft.com/en-us/library/dn254985%28v=vs.85%29.aspx)
+            if (!window.MutationObserver) {
+                // Now, we just use the fall back icon defined in the widget.css defined in the
+                // plugin.xml. Another possible workaround would be using the deprecated mutation
+                // events API. However, those functions have a huge performance penalty and 
+                // severely slow down dynamic pages.
+                return;
+            }
+
             var self = this;
-            var backgroundStyle = 'rgba(0, 0, 0, 0) url("' + net.jazz.ajax._contextRoot + self._url + '") no-repeat scroll -96px -96px';
+            var backgroundStyle = 'rgba(0, 0, 0, 0) url("' 
+                + net.jazz.ajax._contextRoot 
+                + self._url 
+                + '") no-repeat scroll -96px -96px';
 
             var observer = new MutationObserver(function(mutations) {
                 mutations.filter(function(mutation) {
